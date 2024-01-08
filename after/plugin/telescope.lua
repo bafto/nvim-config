f74bacc3236a63969979a28af6aeec4ca456995f
Hendrik Ziegler <hendrikziegler@outlook.de>
2024-01-08 17:46:56 +0100

    telescope ignores

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,17 @@
+require('telescope').setup {
+	defaults = {
+		file_ignore_patterns = {
+			'node_modules/*',
+			-- DDP-Projekt specific
+			'llvm%-project/*'
+		}
+	},
+}
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>grep', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ")})
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
