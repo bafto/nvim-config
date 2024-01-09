3d80e2420d8b26c585a4eeb0450a153a52c3aa36
Hendrik Ziegler <hendrikziegler@outlook.de>
2024-01-09 19:44:48 +0100

    lsp keymapping

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,7 +1,7 @@
-vim.keymap.set("i", "<C-Tab>", "copilot#Accept(\"\")", {
-	expr = true,
-	replace_keycodes = false,
-})
+--vim.keymap.set("i", "<Tab>", "copilot#Accept(\"\")", {
+--	expr = true,
+--	replace_keycodes = false,
+--})
 vim.g.copilot_no_tab_map = true
 -- cylce next and previous
 vim.keymap.set("i", "<C-W>", "copilot#Next()", { expr = true })
