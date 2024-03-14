c33d87afd734fa70a481893090db2dabc065bc07
hziegler <Hendrik.Ziegler@doubleslash.de>
2024-03-14 08:45:03 +0100

    telescope grep reconfig

--- lua/bafto/lazy/telescope.lua
+++ lua/bafto/lazy/telescope.lua
@@ -33,10 +33,9 @@ return {
 			local builtin = require('telescope.builtin')
 
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-			vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Find git files' })
-			vim.keymap.set('n', '<leader>grep', function()
-				builtin.grep_string({ search = vim.fn.input("Grep > ") })
-			end, { desc = 'Grep' })
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+			vim.keymap.set('n', '<leader>gr', builtin.grep_string, { desc = 'Grep word under cursor' })
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
 		end
 	},
 	{
