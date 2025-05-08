c3537e345ae52de072bcc043fad245068a98d0b8
bafto <hendrikziegler@outlook.de>
2025-05-08 14:18:00 +0200

    dartls

--- lua/bafto/lazy/lsp.lua
+++ lua/bafto/lazy/lsp.lua
@@ -173,6 +173,10 @@ return {
 				}
 			}
 
+			lspconfig.dartls.setup {
+				cmd = { 'dart', 'language-server', '--protocol=lsp' }
+			}
+
 			-- DDP setup
 			vim.filetype.add({
 				extension = {
