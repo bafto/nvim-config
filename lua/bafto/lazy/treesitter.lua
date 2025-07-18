6865af6a9aef20a31b8847af564c5b48460216d9
Hendrik Ziegler <hendrikziegler@outlook.de>
2025-07-18 20:49:54 +0200

    updated treesitter

--- lua/bafto/lazy/treesitter.lua
+++ lua/bafto/lazy/treesitter.lua
@@ -2,39 +2,35 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 
 	build = ":TSUpdate",
+	branch = "main",
+	version = false,
 
-	dependencies = {
-		'nvim-treesitter/playground',
-	},
+	opts = {
+		-- A list of parser names, or "all" (the five listed parsers should always be installed)
+		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "java", "javascript", "jsdoc", "typescript", "bash", "cmake", "cpp", "dockerfile", "gitignore", "gitcommit", "gomod", "gosum", "html", "json", "llvm", "make", "python", "scala", "sql", "yaml", "markdown_inline", "terraform", "vue", "prolog" },
+		ignore_install = {},
+		modules = {},
 
-	config = function()
-		require 'nvim-treesitter.configs'.setup {
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "java", "javascript", "jsdoc", "typescript", "bash", "cmake", "cpp", "dockerfile", "gitignore", "gitcommit", "gomod", "gosum", "html", "json", "llvm", "make", "python", "scala", "sql", "yaml", "markdown_inline", "terraform", "vue", "prolog" },
-			ignore_install = {},
-			modules = {},
+		-- Install parsers synchronously (only applied to `ensure_installed`)
+		sync_install = false,
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
+		-- Automatically install missing parsers when entering buffer
+		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+		auto_install = true,
 
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
+		highlight = {
+			enable = true,
 
-			highlight = {
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-			playground = {
-				enable = true,
-				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-				persist_queries = false, -- Whether the query persists across vim sessions
-			},
-		}
-	end,
+			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+			-- Using this option may slow down your editor, and you may see some duplicate highlights.
+			-- Instead of true it can also be a list of languages
+			additional_vim_regex_highlighting = false,
+		},
+		playground = {
+			enable = true,
+			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+			persist_queries = false, -- Whether the query persists across vim sessions
+		},
+	}
 };
